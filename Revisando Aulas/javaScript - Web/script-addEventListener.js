b90422559c885fbbb3ef09f2ce6ae64622bb9663
Alessandro Pedroso <alessandropedrosoti@gmail.com>
2024-04-02 05:50:00 -0300

    eventos

--- Revisando Aulas/javaScript - Web/script-addEventListener.js
+++ Revisando Aulas/javaScript - Web/script-addEventListener.js
@@ -1,6 +1,5 @@
 
 const inputValue = document.querySelector("#main-button");
-console.log(inputValue);
 const select = document.querySelector("select");
 const buttonAdd = document.querySelector(".main-button");
 
@@ -11,9 +10,17 @@ function troqueiOValor(event){
 
 }
 
+const troqueiOValorDoInput = (qualQuerNomeInput) =>{
 
+    console.log("troquei o valor do input");
+    console.log(qualQuerNomeInput);
+}
 
-select.addEventListener("change",troqueiOValor);
+// buttonAdd.addEventListener("click",troqueiOValor);
+buttonAdd.addEventListener("click",function(qualQuerNomeInput){
+    console.log(qualQuerNomeInput);
+});
+// inputValue.addEventListener("keypress",troqueiOValor);  
 
 
 

deefc40c6d4348eedab117b9fe3c476ccdfdbe8d
Your Name <alessanrospedroso@outlook.com>
2024-05-16 08:39:39 -0300

    aula sobre match curso devClub

---
+++ Revisando Aulas/JavaScript - pt.3/1.Math.js
@@ -0,0 +1,34 @@
+/**
+ Math -> Matemática
+ - pow(2,2) / potência / 2² = 4
+ - sqrt(25,2) / raiz / v25
+ - PI -> 3,14
+ - ceil / teto -> arredondar pra cima
+ - floor / chão -> arredondar pra baixo
+
+ -random() / número aleatório entre 0 e 1
+ */
+
+ const result = Math.pow(2,3)
+ console.log("Potência:",result)
+
+ const resultSqrt = Math.sqrt(25,5)
+ console.log("Raiz quadrada:",resultSqrt)
+
+ const resultPi = Math.PI
+ console.log("PI:",resultPi)
+
+ const resultCeil = Math.ceil(3.14)
+ console.log("Ceil arredonda pra cima:",resultCeil)
+
+ const resultFloor = Math.floor(3.18)
+ console.log("Floor arredonda pra baixo:",resultFloor)
+
+ const resultRandoom = Math.random()
+ console.log("Número aleatorio entre 0 e 1:",resultRandoom)
+
+
+
+ 
+
+

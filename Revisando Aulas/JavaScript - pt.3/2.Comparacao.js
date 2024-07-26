d77cf7ab0a06d0e7bf6fbe27e613847f3bb0b3ee
Your Name <alessanrospedroso@outlook.com>
2024-07-26 17:52:07 -0300

    comparação

---
+++ Revisando Aulas/JavaScript - pt.3/2.Comparacao.js
@@ -0,0 +1,17 @@
+const firstNumber = 30
+const seccondNumber = 40
+
+
+console.log(firstNumber == seccondNumber) //retorna verdadeiro ou falso
+
+const firstNumberType = 30
+const seccondNumberType = "30"
+
+
+console.log(firstNumberType === seccondNumberType) //retorna verdadeiro ou falso o tipo
+
+// if (firstNumber == seccondNumber) {
+//     console.log("são iguais")
+// } else {
+//     console.log("Não são iguais")
+// }

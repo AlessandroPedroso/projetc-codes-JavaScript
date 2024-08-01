911e06f72b1038c3b00b150dfc4cc694137ade64
Your Name <alessanrospedroso@outlook.com>
2024-08-01 14:17:45 -0300

    estudando switch

---
+++ Revisando Aulas/JavaScript - pt.3/SwitchCase.js
@@ -0,0 +1,19 @@
+//CONTROLADOR DE FLUXO
+
+const temperatura = 20
+
+switch (temperatura) {
+    case 10:
+        console.log('Ta meio frio')
+        break;
+    case 20:
+        console.log('Ta meio quente')
+        break;
+    case 30:
+        console.log('Ta quente')
+        break;
+
+    default:
+        console.log('Não sei dizer')
+        break;
+}
---
+++ Revisando Aulas/JavaScript - pt.3/operadorTernario.js
@@ -0,0 +1,19 @@
+const salary = 15000
+
+// if (salary < 3000) {
+//     console.log('o colaborador é junior')
+// } else {
+//     console.log('Ele é Senior')
+// }
+
+//operador ternário
+
+// IF => salary < 3000 && console.log('O colaborador é Junior')//entra somente se for verdadeiro
+
+
+// IF/ELSE => salary < 3000 ? console.log('O colaborador é Junior') : console.log('Ele é Senior')
+
+//ELSE IF => 
+salary < 3000 ? console.log("O colaborador é Junior"): salary >= 3000 && salary < 10000 ? console.log('Ele é Senior') : console.log("Ele é diretor")
+
+

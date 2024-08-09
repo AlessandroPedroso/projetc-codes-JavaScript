4c920b30f1c1caa221f8ae519bcf2859b73ca7db
Your Name <alessanrospedroso@outlook.com>
2024-08-09 15:24:45 -0300

    comando for

---
+++ Revisando Aulas/JavaScript - pt.3/for.js
@@ -0,0 +1,23 @@
+//1) INICIALIZAÇÃO - Criar uma variável e colocar um valor inicial pra ela
+
+//2) CONDIÇÃO - Enquanto for true, o laço continuará iterando.
+
+//3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que o nosso laço
+
+//aumentando
+for (let i = 0; i < 15; i++ ){
+    //console.log(i)
+}
+
+//diminuindo
+for (let j = 20; j > 2; j-- ){
+    //console.log(j)
+}
+
+const names = ['Maria', 'José', 'João', 'Mario', 'Jubileu']
+
+for (let i = 0; i < names.length; i++){
+    console.log(names[i])
+}
+
+

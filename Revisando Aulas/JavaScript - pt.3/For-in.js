184029e4e45e65cad79816740515e8f0001eca5f
Your Name <alessanrospedroso@outlook.com>
2024-08-23 09:48:56 -0300

    estudando sobre for in

---
+++ Revisando Aulas/JavaScript - pt.3/For-in.js
@@ -0,0 +1,5 @@
+/*
+    Estrutura de Repetição - LOOP
+        - FOR IN
+*/
+
---
+++ Revisando Aulas/JavaScript - pt.3/For-of.js
@@ -0,0 +1,11 @@
+/*
+    Estrutura de Repetição - LOOP
+        - FOR OF
+*/
+
+const myName = 'Rodolfo'
+const users = ['João','Maria', 'Letícia']
+
+for (const element of users) {
+    console.log(element)
+}

766efda56f08fdeb1a8d444def789043b482eb57
Your Name <alessanrospedroso@outlook.com>
2024-08-23 13:34:24 -0300

    iterando com o for in

--- Revisando Aulas/JavaScript - pt.3/For-in.js
+++ Revisando Aulas/JavaScript - pt.3/For-in.js
@@ -3,3 +3,14 @@
         - FOR IN
 */
 
+const users = { name: 'Alessandro', age: 33, street: 'Rua dos Bobos' }
+
+
+
+for (const key in users) {
+    console.log(users[key])
+}
+
+for (const key in users) {
+    console.log(`${key}: ${users[key]}`)
+}

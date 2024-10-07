cf753b0b931ce289074aba39bbdb994e357c21df
Your Name <alessanrospedroso@outlook.com>
2024-10-07 17:49:27 -0300

    enum

--- Revisando Aulas/JavaScript - pt.3/jokenpow/script.js
+++ Revisando Aulas/JavaScript - pt.3/jokenpow/script.js
@@ -5,13 +5,26 @@ const machineScore = document.getElementById('machine-score');
 let humanScoreNumber = 0;
 let machineScoreNumber = 0;
 
+
+/*
+    humanScoreNumber -> Camel Case
+    GAME_OPTIONS     -> Snake Case
+*/
+
+//ENUM
+const GAME_OPTIONS = {
+    ROCK: 'rock',
+    PAPER: 'paper',
+    SCISSORS: 'scissors'
+}
+
 const playHuman = (humanChoice) => {
 
     playTheGame(humanChoice, playMachine());
 }
 
 const playMachine = () => {
-    const choice = ['rock', 'paper', 'scissors'];
+    const choice = [GAME_OPTIONS.ROCK, GAME_OPTIONS.PAPER, GAME_OPTIONS.SCISSORS];
     const randomNumber = Math.floor(Math.random() * 3);
 
     return choice[randomNumber];
@@ -22,9 +35,9 @@ const playTheGame = (human, machine) => {
     
     if (human === machine) {
         result.innerHTML = "Deu empate!"
-    } else if ((human === 'paper' && machine === 'rock') ||
-        (human === 'rock' && machine === 'scissors') ||
-        (human === 'scissors' && machine === 'paper')) {
+    } else if ((human === GAME_OPTIONS.PAPER && machine === GAME_OPTIONS.ROCK) ||
+        (human === GAME_OPTIONS.ROCK && machine === GAME_OPTIONS.SCISSORS) ||
+        (human === GAME_OPTIONS.SCISSORS && machine === GAME_OPTIONS.PAPER)) {
         result.innerHTML = 'Você ganhou!'
         humanScoreNumber++
         humanScore.innerHTML = humanScoreNumber

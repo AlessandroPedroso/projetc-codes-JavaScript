6f05635a0b0cb74f4e8dc70b23a7c9b1353488b4
Your Name <alessanrospedroso@outlook.com>
2025-02-24 17:25:44 -0300

    map, reduce e filter

--- MAP-REDUCE-FILTER/exerc-map-reduce-filter.js
+++ MAP-REDUCE-FILTER/exerc-map-reduce-filter.js
@@ -1,35 +1,50 @@
 const companies = [
-
-    {name:'Samsung', marketValue:50, CEO:'Kim Hyun Suk', foundedOn: 1938},
-    {name:'Microsoft', marketValue:415, CEO:'Satya Nadella', foundedOn: 1975},
-    {name:'Intel', marketValue:117, CEO:'Brian Krzanich', foundedOn: 1968},
-    {name:'Facebook', marketValue:383, CEO:'Mark Zuckerberg', foundedOn: 2004},
-    {name:'Spotify', marketValue:30, CEO:'Daniel Ek', foundedOn: 2006},
-    {name:'Apple', marketValue:845, CEO:'Tim Cook', foundedOn: 1976}
-
+  { name: "Samsung", marketValue: 50, CEO: "Kim Hyun Suk", foundedOn: 1938 },
+  {
+    name: "Microsoft",
+    marketValue: 415,
+    CEO: "Satya Nadella",
+    foundedOn: 1975,
+  },
+  { name: "Intel", marketValue: 117, CEO: "Brian Krzanich", foundedOn: 1968 },
+  {
+    name: "Facebook",
+    marketValue: 383,
+    CEO: "Mark Zuckerberg",
+    foundedOn: 2004,
+  },
+  { name: "Spotify", marketValue: 30, CEO: "Daniel Ek", foundedOn: 2006 },
+  { name: "Apple", marketValue: 845, CEO: "Tim Cook", foundedOn: 1976 },
 ];
 
-const add10PorcentMarketValue = companies =>{
-    companies.marketValue += companies.marketValue / 10
-    return companies
-}
+const add10PorcentMarketValue = (companies) => {
+  companies.marketValue += companies.marketValue / 10;
+  return companies;
+};
 
-const oldCompanies = company => company.foundedOn < 2000
+const oldCompanies = (company) => company.foundedOn < 2000;
 
-const sumCompanies = (acc,count) => acc + count.marketValue
+const sumCompanies = (acc, count) => acc + count.marketValue;
 
 const newCompanies = companies
-    .map(add10PorcentMarketValue)
-    .filter(oldCompanies)
-    .reduce(sumCompanies,0)
+  .map(add10PorcentMarketValue)
+  .filter(oldCompanies)
+  .reduce(sumCompanies, 0);
 
-console.log(newCompanies)
+const sumTotal = newCompanies.reduce(sumCompanies, 0);
+
+const objecNew = {
+  ...newCompanies,
+  sumTotal,
+};
+
+console.log(objecNew);
 
 /* Exercicio utilizando MAP */
 // const newCompanies = companies.map(companies =>{
 //     let resultPorcentagem = companies.marketValue + ((companies.marketValue/100) * 10)
-    
-//     const companiesList = 
+
+//     const companiesList =
 //      { name: companies.name, marketValue: resultPorcentagem, CEO: companies.CEO, foundedOn: companies.foundedOn}
 
 //     return companiesList
